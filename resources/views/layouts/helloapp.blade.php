4374c9de1a48bb90880f347da59b2369bf50701d
s-hamada2640 <hsat2640@gmail.com>
2021-06-18 02:21:26 +0900

    エラー

---
+++ resources/views/layouts/helloapp.blade.php
@@ -0,0 +1,14 @@
+<html>
+
+<head>
+    <title>@yield</title>
+    <style>
+        body {
+            font-size: 16pt;
+            color: #999;
+            margin: 5px;
+        }
+        
+        h1 {
+            font-size:
+        }
